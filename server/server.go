1b00ee94f924c93f85dc7616f459e6bfb400ff66
Quoc Vu <quocvu2640@gmail.com>
2024-11-20 15:28:50 -0500

    fix: deployment issue
    Added CORS
    Added Deploy to workflow
    Changed swagger API URL

--- server/server.go
+++ server/server.go
@@ -14,6 +14,7 @@ import (
 
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"gorm.io/gorm"
 )
 
@@ -79,6 +80,18 @@ func (m *Server) Stop(ctx context.Context) error {
 func (m *Server) NewRouter() {
 	// Creates a chi.Router for the server
 	r := chi.NewRouter()
+
+	// Include CORS handler to fix CORS error in API calls.
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	r.Use(middleware.Logger)
 
