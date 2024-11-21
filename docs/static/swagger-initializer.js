62aa0ab97ca3e249defb443e7d4d4d0a6fa09fdf
Quoc Vu <quocvu2640@gmail.com>
2024-11-21 17:34:27 -0500

    fix: fixed swagger url

--- docs/static/swagger-initializer.js
+++ docs/static/swagger-initializer.js
@@ -3,8 +3,8 @@ window.onload = function () {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "http://localhost:5050/api/v1/openapi.yaml",
-    // url: "https://api.plainrandom.com/api/v1/openapi.yaml",
+    // url: "http://localhost:5050/api/v1/openapi.yaml",
+    url: "https://api.plainrandom.com/api/v1/openapi.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
