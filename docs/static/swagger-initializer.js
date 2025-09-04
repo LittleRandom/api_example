5e7e002a2d3329db76c12ed3f9d3dd9333debbaa
Quoc Vu <quocvu2640@gmail.com>
2025-09-04 11:08:52 -0400

    fix: update swagger CORs

--- docs/static/swagger-initializer.js
+++ docs/static/swagger-initializer.js
@@ -3,18 +3,13 @@ window.onload = function () {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "http://localhost:5050/api/v1/openapi.yaml",
-    // url: "https://api.plainrandom.com/api/v1/openapi.yaml",
-    dom_id: '#swagger-ui',
+    // url: "http://localhost:5050/api/v1/openapi.yaml",
+    url: "https://api.plainrandom.com/api/v1/openapi.yaml",
+    dom_id: "#swagger-ui",
     deepLinking: true,
-    presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
-    ],
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
-    ],
-    layout: "StandaloneLayout"
+    presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+    plugins: [SwaggerUIBundle.plugins.DownloadUrl],
+    layout: "StandaloneLayout",
   });
 
   //</editor-fold>
